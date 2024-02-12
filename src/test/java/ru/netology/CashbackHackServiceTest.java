fbee30968ecb24721f43554bf546852f2cf5625e
Nelly Shigapova <nelly-shig@yandex.ru>
2024-02-12 23:24:04 +0400

    Use junit4

--- src/test/java/ru/netology/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -1,23 +1,23 @@
 package ru.netology;
 
-import org.testng.annotations.Test;
-import org.testng.Assert;
+import org.junit.Test;
+import org.junit.Assert;
 
 public class CashbackHackServiceTest {
 
     @Test
     public void testRemain() {
         CashbackHackService cashbackHackService = new CashbackHackService();
-        int actual = 100;
-        int expected = cashbackHackService.remain(900);
-        Assert.assertEquals(actual, expected);
+        int expected  = 100;
+        int actual = cashbackHackService.remain(900);
+        Assert.assertEquals(expected, actual);
     }
 
     @Test
     public void testRemain2() {
         CashbackHackService cashbackHackService = new CashbackHackService();
-        int actual = 0;
-        int expected = cashbackHackService.remain(1000);
-        Assert.assertEquals(actual, expected);
+        int expected = 0;
+        int actual = cashbackHackService.remain(1000);
+        Assert.assertEquals(expected, actual);
     }
 }
